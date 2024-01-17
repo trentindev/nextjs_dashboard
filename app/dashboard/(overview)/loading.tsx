e142461aa39add0cbf8e3281f2091f29439927a8
Laurent Dislaire <siladire@free.fr>
2024-01-17 10:24:26 +0100

    End of chapter 7:chapter 9 suspense part1

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,7 +1,9 @@
 /*loading.tsx is a special Next.js file
  built on top of Suspense, it allows you to create fallback
  UI to show as a replacement while page content loads.*/
+
+import DashboardSkeleton from '@/app/ui/skeletons';
  
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <DashboardSkeleton />;
 }
